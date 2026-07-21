e48d5eb4c3fb4cba81fb790c60d4f6277c8580a0
kumawatak421-rgb <kumawatak421@gmail.com>
2026-07-22 03:21:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ output = 'dental_disease_model.h5'
 if not os.path.exists(output):
     gdown.download(url, output, quiet=False)
 import streamlit as st
-import tflite_runtime.interpreter as tflite
 
 import cv2
 import numpy as np
